a9d375fab80a1e1416ed046555da5415051bbae5
baquer <baquerali@icloud.com>
2018-06-03 16:02:12 +0530

    update

--- BinarySearch/BinarySearch.playground/Contents.swift
+++ BinarySearch/BinarySearch.playground/Contents.swift
@@ -6,7 +6,10 @@ import UIKit
 // function of binary search
 
 
-// Binary Search: Search a sorted array by repeatedly dividing the search interval in half. Begin with an interval covering the whole array. If the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half. Otherwise narrow it to the upper half. Repeatedly check until the value is found or the interval is empty.
+// Binary Search: Search a sorted array by repeatedly dividing the search interval in half.
+//Begin with an interval covering the whole array. If the value of the search key is less than the item in the middle of the interval,
+//narrow the interval to the lower half. Otherwise narrow it to the upper half.
+//Repeatedly check until the value is found or the interval is empty.
 
 func binarySearch(numbers: [Int], first:Int, last:Int, element:Int ) -> Int {
     
