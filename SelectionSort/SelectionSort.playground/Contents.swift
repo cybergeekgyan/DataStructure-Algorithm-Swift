5f4b63bec482b239ac5d83a68f32546cc637ee95
baquer <baquerali@icloud.com>
2018-06-07 02:55:35 +0530

    Selection Sort

---
+++ SelectionSort/SelectionSort.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+selectionSort(list, <)
+selectionSort(list, >)
+
+list
+
